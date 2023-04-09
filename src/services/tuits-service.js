013b3d331e2a3adfb4bbebceb2ee1f8a216e22b3
Karthik J Bharadwaj <37576167+Karthik1235@users.noreply.github.com>
2023-04-09 14:55:19 -0400

    a9 Mongo sp23

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_BASE = process.env.REACT_APP_API_BASE;
+const API_BASE = process.env.REACT_APP_API_BASE_Heroku || process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
@@ -21,3 +21,4 @@ export const updateTuit = async (tuit) => {
         .put(`${TUITS_API}/${tuit._id}`, tuit);
     return tuit;
 }
+h
