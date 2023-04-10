dcba449a2a3506ffc81ba10b465359714580fc68
Karthik J Bharadwaj <37576167+Karthik1235@users.noreply.github.com>
2023-04-09 21:24:02 -0400

    Fixed new tuit image

--- src/tuiter/tuits/tuit-item.js
+++ src/tuiter/tuits/tuit-item.js
@@ -31,12 +31,6 @@ const checkProfileImage = (post) => {
     return <img className={`profile-pic float-start rounded-circle`} src={`../../../tuiter/images/NASA_logo.png`}/>;
 }
 
-const checkHandle = (post) => {
-    if(post.handle!=""){
-        return {post.handle};
-    }
-    return `NASA`;
-}
 
 const checkLink = (post) => {
     if(post.hasLink){
@@ -87,12 +81,7 @@ const TuitItem = (
                 <i className="bi bi-x-lg float-end"
                    onClick={() => deleteTuitHandler(post._id)}></i>
                 <a className="float-end text-secondary" href="#"> <i className="fa-solid fa-ellipsis"></i> </a>
-                <div className="ps-2">
-                    <b>{post.userName}</b>
-                    {checkIfVerified(post)}
-                    <span className = "ps-1 text-secondary">
-                    @{checkHandle(post)} {getPostsTiming(post)}
-                </span>
+                <div className="ps-2"> <b>{post.userName}</b> {checkIfVerified(post)} <span className = "ps-1 text-secondary">@{post.handle} {getPostsTiming(post)}</span>
                 </div>
                 <div className="ps-2 pt-2 pe-1">
                     <p className="ps-1"> {renderTuit(post)}
